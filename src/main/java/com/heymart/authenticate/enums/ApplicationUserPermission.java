029d261e3cd11feef5e5d73c531fa5a00f15e200
DhafinFK <dhafin.kamal@gmail.com>
2024-05-25 18:33:49 +0700

    [FEAT] added balance permissions

--- src/main/java/com/heymart/authenticate/enums/ApplicationUserPermission.java
+++ src/main/java/com/heymart/authenticate/enums/ApplicationUserPermission.java
@@ -8,7 +8,14 @@ public enum ApplicationUserPermission {
     COUPON_READ("coupon:read"),
     COUPON_CREATE("coupon:create"),
     COUPON_UPDATE("coupon:update"),
-    COUPON_DELETE("coupon:delete");
+    COUPON_DELETE("coupon:delete"),
+    USER_BALANCE_CREATE("balance:ub_create"),
+    SUPERMARKET_BALANCE_CRATE("balance:sb_create"),
+    USER_BALANCE_READ("balance:ub_read"),
+    SUPERMARKET_BALANCE_READ("balance:sb_read"),
+    USER_BALANCE_UPDATE("balance:ub_update"),
+    SUPERMARKET_BALANCE_UPDATE("balance:sb_update"),
+    TRANSACTION_READ("transaction:read");
 
     private final String permission;
 
--- src/main/java/com/heymart/authenticate/enums/ApplicationUserRole.java
+++ src/main/java/com/heymart/authenticate/enums/ApplicationUserRole.java
@@ -12,8 +12,10 @@ import static com.heymart.authenticate.enums.ApplicationUserPermission.*;
 public enum ApplicationUserRole {
 
     ADMIN(Sets.newHashSet(AUTH_CREATE_MANAGER, COUPON_READ)),
-    CUSTOMER(Sets.newHashSet(COUPON_READ)),
-    MANAGER(Sets.newHashSet(COUPON_CREATE, COUPON_READ, COUPON_DELETE, COUPON_UPDATE));
+    CUSTOMER(Sets.newHashSet(COUPON_READ, USER_BALANCE_CREATE, USER_BALANCE_READ, USER_BALANCE_UPDATE)),
+    MANAGER(Sets.newHashSet(
+            COUPON_CREATE, COUPON_READ, COUPON_DELETE, COUPON_UPDATE, SUPERMARKET_BALANCE_CRATE,
+            SUPERMARKET_BALANCE_READ, SUPERMARKET_BALANCE_UPDATE));
 
 
     private final Set<ApplicationUserPermission> permissions;
